4eef0a2f06de2203739e4e3677b4ddfeea7f7db6
vanya1511 <58367635+vanya1511@users.noreply.github.com>
2020-04-20 16:26:51 +0400

    Initial commit

---
+++ app/src/main/java/com/example/a341/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.a341;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    Bitmap bmp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new AsyncRequest().execute();
+    }
+    class AsyncRequest extends AsyncTask<Void,Void,Void>{
+        ImageView imgView;
+        InputStream stream;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                URL url = new URL("https://voip.mephi.ru/public/photos/9108_cps.jpg");
+                stream = (InputStream) url.getContent();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            bmp = BitmapFactory.decodeStream(stream);
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            imgView = findViewById(R.id.ImageView01);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            imgView.setImageBitmap(bmp);
+            super.onPostExecute(aVoid);
+        }
+    }
+}
+
+
+
